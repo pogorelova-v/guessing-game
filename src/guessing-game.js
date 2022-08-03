6e6f4cac28770000bf882982eb4bd2869bfb3a5a
pogorelova-v <gorelikovavn@gmail.com>
2022-08-03 09:06:06 +0300

    feat: this is very hard, but i complited!

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
     }
 
+    setRange(min, max) {
+       this.min = min;
+       this.max = max;
+    }
+    
     guess() {
-
+        this.num = Math.round((this.min + this.max)/2);
+        return this.num;
     }
 
     lower() {
-
+        this.max = this.num;
     }
 
     greater() {
-
+        this.min = this.num;
     }
 }
 
